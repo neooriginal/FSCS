58565db8277bed1f4f9c197ef9ec01ed07af94a7
Neo <54811660+Neotastisch@users.noreply.github.com>
2025-03-07 14:50:49 +1000

    beta password & landing page

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+//temporary password auth for beta
+const basicAuth = require('express-basic-auth');
+if (process.env.betaPassword) {
+  app.use(basicAuth({
+    users: { 'beta': process.env.betaPassword }, // User: admin, Password: password123
+    challenge: true,  // This will trigger the built-in browser authentication popup
+    realm: 'Beta Testing' // Optional: This is the name that will show in the popup prompt
+  }));
+}
+//
+
 // Increase JSON payload size limit for file uploads
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
